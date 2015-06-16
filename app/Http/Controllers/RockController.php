3a2fceef7ea8206622e8f6c9a3fc616ddba75d16
joseVargas <josepavaa@hotmail.com>
2015-06-16 17:23:15 -0600

    push and consume

--- app/Http/Controllers/RockController.php
+++ app/Http/Controllers/RockController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Queue\InteractsWithQueue;
+/*use Illuminate\Queue\InteractsWithQueue;*/
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redirect;
 use App\Song;
+
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
 
 class RockController extends Controller
 {
@@ -23,11 +27,34 @@ class RockController extends Controller
         return view('rock.index', compact('songs'));
     }
 
+    public function push($data){
+        $exchange = 'router';
+        $queue = 'msgs';
+
+        $conn = new AMQPConnection('localhost', '5672', 'guest', 'guest', '/');
+        $ch = $conn->channel();
+        $ch->queue_declare($queue, false, true, false, false);
+
+        $ch->exchange_declare($exchange, 'direct', false, true, false);
+
+        $ch->queue_bind($queue, $exchange);
+
+        $msg_body = $data;//implode(' ', array_slice($argv, 1));
+        $msg = new AMQPMessage($msg_body, array('content_type' => 'text/plain', 'delivery_mode' => 2));
+        $ch->basic_publish($msg, $exchange);
+
+        $ch->close();
+        $conn->close();
+
+    }
+
+
     public function send($id)
     {
         $song = \App\Song::find($id);
-        Queue::push('laravel', $song->url_source);
-        return redirect('songs');
+        //\Queue::push($song->url_source);
+        $this->push($song->url_source);
+        return redirect('rocks');
     }
 
    
